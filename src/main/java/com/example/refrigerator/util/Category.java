6bb150733e965d366d87080208cd8173c32b02c2
Ryuhyis <skyblue22756@gmail.com>
2023-02-18 14:47:45 +0900

    :art: Add kind of Category

--- src/main/java/com/example/refrigerator/util/Category.java
+++ src/main/java/com/example/refrigerator/util/Category.java
@@ -6,8 +6,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    FRUIT("과일",1),
-    MEET("육류",2)
+    VEGETABLE("채소",1),
+    FRUIT("과일",2),
+    CEREALS("곡류", 3),
+    NUT("견과류",4),
+    MEAT("육류",5),
+    SEAFOOD("수산물",6),
+    NOODLE("면류",7),
+    SAUCE("소스",8),
+    MILK("유제품",9),
+    BEVERAGE("음료",10),
+    DISH("요리/반찬",11),
+    INSTANT("인스턴트",12),
+    DESERT("디저트",13),
+    ETC("기타",14)
     ;
 
     private final String CategoryName;
