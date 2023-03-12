be2d0c85f458579bdedfaae9fef9cae5c445febe
Ryuhyis <skyblue22756@gmail.com>
2023-03-13 01:24:54 +0900

    :art: Response 형태 변경

---
+++ src/main/java/com/example/refrigerator/dto/FindPwdDto.java
@@ -0,0 +1,17 @@
+package com.example.refrigerator.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindPwdDto {
+
+    @NotNull
+    private String username;
+}
